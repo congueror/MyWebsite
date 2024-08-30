b1492cc43ab5fe0ea00eff292b29b66d55660a1a
brain.exe <mariovionikos@gmail.com>
2024-08-30 10:21:54 +0300

    Lib Search Bar Done

--- Libraries/MathLibrary/main.js
+++ Libraries/MathLibrary/main.js
@@ -28,6 +28,12 @@ function getObjData(obj, id) {
 
 var language = "en_us";
 var params = new URLSearchParams(window.location.search);
+/**
+ * @type {Set<string>}
+ */
+var problemTags = new Set();
+var problemsInPage = 5;
+var page = 1;
 var totalProblems = 0;
 var wipProblems = 0;
 
@@ -74,18 +80,133 @@ function fillDefault() {
         addContent(o, "", 0);
     });
 
-    let problems = document.getElementById('problems');
-    problems.innerHTML += '<h1>Search All Problems by Tag</h1>';
-
-    problems.innerHTML += `<input id="searchbar" class="live_search" onkeyup="search_tag()" type="text" name="search" placeholder="Filter tags... Use &(AND) and |(OR) for more advanced filtering."> <br><br>`;
-
-    let tags = new Set();
-    let index = 0;
+    let problemsArray = [];
     DATA[language].forEach(o => {
-        tags = tags.union(parseTags(o));
-        index += addProblems(o, index);
+        problemTags = problemTags.union(parseTags(o));
+        problemsArray = problemsArray.concat(getAllProblems(o));
     });
 
+    createTagSearchBar();
+
+    let count = 0;
+    for (let i = 0; i < problemsInPage; i++) {
+        let pp = problemsArray[(page - 1) * problemsInPage];
+        if (problemsArray[i])
+            addProblem(problemsArray[i], count++);
+    }
+
+    createPageButtons(problemsArray);
+}
+
+function createTagSearchBar() {
+    let body_div = document.getElementById('body_div');
+    let problems = document.getElementById('problems');
+    let s1 = document.createElement("h1");
+    s1.innerHTML = `Search All Problems by Tag`;
+    let s2 = document.createElement("input");
+    s2.id = "searchbar";
+    s2.className = "live_search";
+    s2.setAttribute("onkeyup", "searchTag()");
+    s2.type = "text";
+    s2.name = "search";
+    s2.placeholder = "Filter tags... Use &(AND) and |(OR) for more advanced filtering.";
+
+    body_div.insertBefore(s1, problems);
+    body_div.insertBefore(s2, problems);
+}
+
+function createPageButtons(arr) {
+    let body_div = document.getElementById('body_div');
+    let sDiv = document.createElement("div");
+    sDiv.id = "page_div";
+    sDiv.style.display = "flex";
+    sDiv.style.alignItems = "center";
+    sDiv.style.justifyContent = "center";
+    sDiv.onchange = () => {
+        problemsInPage = document.getElementById("problemsInPage").value;
+        page = document.getElementById("page").value;
+        document.getElementById("problems").innerHTML = "";
+
+        let count = (page - 1) * problemsInPage;
+        for (let i = 0; i < problemsInPage; i++) {
+            let pp = arr[(page - 1) * problemsInPage + i];
+            if (pp)
+                addProblem(pp, count++);
+        }
+
+        MathJax.typeset();
+    }
+
+    let s1 = document.createElement("button");
+    s1.className = "next_page";
+    s1.style.backgroundImage = "url(../../icons/left_arrow.png)";
+    s1.setAttribute("onclick", "nextPageClick(-1)");
+    sDiv.appendChild(s1);
+
+    let str = `Showing <input id="problemsInPage" type="number" value="5" min="1" max="$1" style="width:35px"/> problems in page <input id="page" type="number" value="1" min="1" max="$2" style="width:35px"/> of $2`;
+    sDiv.insertAdjacentHTML("beforeend", str.replace("$1", arr.length).replaceAll("$2", Math.ceil(arr.length / 5)));
+
+    let s2 = document.createElement("button");
+    s2.className = "next_page";
+    s2.setAttribute("onclick", "nextPageClick(1)");
+    sDiv.appendChild(s2);
+
+    body_div.appendChild(sDiv);
+}
+
+function searchTag() {
+    let body = document.getElementById("body_div");
+    let page = document.getElementById("page_div");
+    let problems = document.getElementById("problems");
+    let searchbar = document.getElementById("searchbar");
+    let prompt = searchbar.value;
+    
+    let candidates = new Set();
+    problemTags.forEach((t) => {
+        if (t.includes(prompt)) {
+            candidates.add(t);
+        }
+    });
+
+    if (candidates.size === 0)
+        return;
+
+    let problemsArray = [];
+    DATA[language].forEach(o => {
+        if (prompt === '') {
+            problemsArray = problemsArray.concat(getAllProblems(o));
+            return;
+        }
+        problemsArray = problemsArray.concat(getAllProblems(o, [], (p) => {
+            if (candidates.intersection(new Set(p.tags)).size !== 0) {
+                return true;
+            }
+            return false;
+        }));
+    });
+
+    problems.innerHTML = "";
+    body.removeChild(page);
+
+    let count = 0;
+    for (let i = 0; i < problemsInPage; i++) {
+        let pp = problemsArray[(page - 1) * problemsInPage];
+        if (problemsArray[i])
+            addProblem(problemsArray[i], count++);
+    }
+
+    createPageButtons(problemsArray);
+
+    MathJax.typeset();
+}
+
+function nextPageClick(inc) {
+    let sDiv = document.getElementById("page_div");
+    let pp = document.getElementById("page");
+    let num = Number.parseInt(pp.value) + inc;
+    if (num <= pp.max && num >= pp.min)
+        pp.value = num;
+    sDiv.dispatchEvent(new Event("change"));
 }
 
 function parseTags(o) {
@@ -104,6 +225,29 @@ function parseTags(o) {
     }
 
     return tags;
+}
+
+/**
+ * 
+ * @param {{id: string, title: string, subunits: Array, problems: Array.<{passage: string, solution: string, tags: Array.<string>}>}} o The object that will be parsed, including its subunits.
+ * @param {Array.<{passage: string, solution: string, tags: Array.<string>}>} arr The array the values will be stored in, should be null.
+ * @param {function({passage: string, solution: string, tags: Array.<string>}):Boolean} predicate
+ * @returns {Array.<{passage: string, solution: string, tags: Array.<string>}>} All problems in the object provided including subunits
+ */
+function getAllProblems(o, arr, predicate) {
+    if (!arr)
+        arr = [];
+    if (o.problems) {
+        o.problems.forEach((p) => {
+            if (!predicate || predicate(p))
+                arr.push(p);
+        });
+    } else if (o.subunits) {
+        o.subunits.forEach(p => {
+            arr = getAllProblems(p, arr, predicate);
+        });
+    }
+    return arr;
 }
 
 function addContent(o, parent, depth, enumerate) {
@@ -194,27 +338,17 @@ function addProblem(o, index) {
     solution.className = "solution";
     div.appendChild(solution);
 
+    if (o.tags) {
+        let footer = document.createElement("footer");
+        let tags = "";
+        o.tags.forEach((s) => {
+            tags += "#" + s;
+        });
+        footer.insertAdjacentText("beforeend", tags);
+        footer.className = "problem_footer";
+        div.appendChild(footer);
+    }
+
     document.getElementById("problems").appendChild(div);
     document.getElementById("problems").appendChild(document.createElement("br"));
-}
-
-/**
- * 
- * @param {{id: string, title: string, subunits: Array, problems: Array.<{passage: string, solution: string, tags: Array.<string>}>}} o The object that will be parsed, including its subunits.
- * @param {number} index The starting index of the problems that will be generated.
- * @returns {number} Amount of problems added.
- */
-function addProblems(o, index) {
-    let i = 0;
-    if (o.problems) {
-        o.problems.forEach((p) => {
-            i++;
-            addProblem(p, index + i - 1);
-        });
-    } else if (o.subunits) {
-        o.subunits.forEach(p => {
-            i += addProblems(p, i + index);
-        });
-    }
-    return i;
 }
